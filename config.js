5a41642cd08b98fb9550ed26a0f320b7ad60f685
Tahara <96952004+taharakuro@users.noreply.github.com>
2024-07-23 16:34:35 +0300

    Update config.js

--- config.js
+++ config.js
@@ -67,64 +67,35 @@ const CONFIG = {
 			id: '3',
 			name: 'YouTube',
 			icon: 'youtube',
-			link: 'https://todoist.com',
+			link: 'https://www.youtube.com',
 		},
 		{
 			id: '4',
-			name: 'Calendar',
-			icon: 'calendar',
-			link: 'https://calendar.google.com/calendar/r',
-		},
-		{
-			id: '5',
 			name: 'Reddit',
-			icon: 'glasses',
+			icon: 'reddit',
 			link: 'https://reddit.com',
 		},
-		{
-			id: '6',
-			name: 'Odysee',
-			icon: 'youtube',
-			link: 'https://odysee.com/',
-		},
-	],
-
-	secondButtonsContainer: [
 		{
 			id: '1',
 			name: 'Music',
 			icon: 'headphones',
 			link: 'https://open.spotify.com',
 		},
+	],
+
+	secondButtonsContainer: [
+
 		{
 			id: '2',
-			name: 'twitter',
-			icon: 'twitter',
-			link: 'https://twitter.com/',
+			name: 'Telegram',
+			icon: 'telegram',
+			link: 'https://web.telegram.org',
 		},
 		{
 			id: '3',
-			name: 'bot',
-			icon: 'bot',
+			name: 'Discord',
+			icon: 'discord',
 			link: 'https://discord.com/app',
-		},
-		{
-			id: '4',
-			name: 'Amazon',
-			icon: 'shopping-bag',
-			link: 'https://amazon.com/',
-		},
-		{
-			id: '5',
-			name: 'Hashnode',
-			icon: 'pen-tool',
-			link: 'https://hashnode.com/',
-		},
-		{
-			id: '6',
-			name: 'Figma',
-			icon: 'figma',
-			link: 'https://figma.com/',
 		},
 	],
 
